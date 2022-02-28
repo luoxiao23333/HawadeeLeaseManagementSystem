0afae532238265be461c3debd57e3e947d27e75f
罗霄 <2362844711@qq.com>
2022-02-28 08:28:38 +0800

    Initial commit

---
+++ src/main/java/com/group5/hawadeeleasemanagementsystem/HawadeeLeaseManagementSystemApplication.java
@@ -0,0 +1,13 @@
+package com.group5.hawadeeleasemanagementsystem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HawadeeLeaseManagementSystemApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HawadeeLeaseManagementSystemApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/group5/hawadeeleasemanagementsystem/HawadeeLeaseManagementSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.group5.hawadeeleasemanagementsystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HawadeeLeaseManagementSystemApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
