dfd902c73e7813d7f4774c8759c51c2e30b0c640
罗霄 <2362844711@qq.com>
2022-02-28 15:32:37 +0800

    添加前端common

--- src/main/java/com/group5/hawadeeleasemanagementsystem/controller/WelcomeController.java
+++ src/main/java/com/group5/hawadeeleasemanagementsystem/controller/WelcomeController.java
@@ -1,12 +1,14 @@
 package com.group5.hawadeeleasemanagementsystem.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+@Controller
 public class WelcomeController {
 
     @RequestMapping(value = "/")
     public ModelAndView welcome(){
-        return new ModelAndView("index");
+        return new ModelAndView("/index");
     }
 }
