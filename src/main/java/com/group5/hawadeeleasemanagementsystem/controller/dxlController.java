b9bdb2308290392725fa981491795430929a9a1a
Niklausf <1635564377@qq.com>
2022-02-28 09:53:29 +0800

    fuck u

--- src/main/java/com/group5/hawadeeleasemanagementsystem/controller/dxlController.java
+++ src/main/java/com/group5/hawadeeleasemanagementsystem/controller/dxlController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/dxl")
 public class dxlController {
     public String dxlTest(){
+        System.out.println("fufufufufufu");
         return "dxlTest";
     }
 }
