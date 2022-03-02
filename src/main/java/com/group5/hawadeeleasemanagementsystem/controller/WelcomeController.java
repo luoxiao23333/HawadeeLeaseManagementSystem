67a6005094b34fcbcdb6bedc3857924715c7c8ea
罗霄 <2362844711@qq.com>
2022-03-02 14:04:13 +0800

    资源配置为从static引用

--- src/main/java/com/group5/hawadeeleasemanagementsystem/controller/WelcomeController.java
+++ src/main/java/com/group5/hawadeeleasemanagementsystem/controller/WelcomeController.java
@@ -4,11 +4,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpSession;
+
 @Controller
 public class WelcomeController {
 
     @RequestMapping(value = "/")
     public ModelAndView welcome(){
-        return new ModelAndView("/index");
+        ModelAndView mv = new ModelAndView("/account/login");
+        mv.addObject("isShowLoginFailed", false);
+        return mv;
     }
+
 }
