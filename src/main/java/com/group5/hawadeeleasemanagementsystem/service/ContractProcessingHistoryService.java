918d1c0ff0115e22ef0bf35ab27a6b17f1bad100
罗霄 <2362844711@qq.com>
2022-03-07 21:43:15 +0800

    添加合同处理趋势图前端

--- src/main/java/com/group5/hawadeeleasemanagementsystem/service/ContractProcessingHistoryService.java
+++ src/main/java/com/group5/hawadeeleasemanagementsystem/service/ContractProcessingHistoryService.java
@@ -6,12 +6,15 @@ import com.group5.hawadeeleasemanagementsystem.domain.ContractHistoryWithUser;
 import com.group5.hawadeeleasemanagementsystem.domain.ContractInfo;
 import com.group5.hawadeeleasemanagementsystem.domain.ContractProcessingHistory;
 import com.group5.hawadeeleasemanagementsystem.domain.ContractWithUser;
+import com.group5.hawadeeleasemanagementsystem.domain.chartData.LineChartData;
+import com.group5.hawadeeleasemanagementsystem.domain.chartData.LineData;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.time.Month;
+import java.time.Year;
+import java.time.YearMonth;
+import java.util.*;
 
 @Service
 public class ContractProcessingHistoryService {
@@ -39,5 +42,53 @@ public class ContractProcessingHistoryService {
 
     void addNewRecord(Integer contractId, Integer status, String reason, Integer processUserId) {
         contractProcessingHistoryDao.addNewRecord(contractId, status, reason, processUserId);
+    }
+
+    LineChartData getProcessingHistoryChartData() throws Exception {
+        List<ContractProcessingHistory> historyList = contractProcessingHistoryDao.getAllHistory();
+        Map<YearMonth, Integer> ApprovedCount = new HashMap<>();
+        Map<YearMonth, Integer> DeniedCount = new HashMap<>();
+        for(ContractProcessingHistory history: historyList){
+            Calendar calendar = Calendar.getInstance();
+            calendar.setTime(history.getCreateDate());
+            final int year = calendar.get(Calendar.YEAR);
+            final int month = calendar.get(Calendar.MONTH);
+            YearMonth date = YearMonth.of(year, month);
+            if(Objects.equals(history.getStatus(), ContractProcessingHistory.Approved)){
+                ApprovedCount.put(date, ApprovedCount.getOrDefault(date, 0));
+                DeniedCount.getOrDefault(date, 0);
+            }else if(Objects.equals(history.getStatus(), ContractProcessingHistory.Denied)){
+                DeniedCount.put(date, DeniedCount.getOrDefault(date, 0));
+                ApprovedCount.getOrDefault(date, 0);
+            }else{
+                throw new Exception("Unsupported Status");
+            }
+        }
+
+        LineChartData lineChartData = new LineChartData();
+        List<String> xAxisData = new ArrayList<>();
+        List<LineData> dataList = new ArrayList<>();
+        LineData approveLineData = new LineData();
+        approveLineData.setLabel("Approved");
+        List<Number> approvedDataset = new ArrayList<>();
+        for(Map.Entry<YearMonth, Integer> record: ApprovedCount.entrySet()){
+            xAxisData.add(record.getKey().toString());
+            approvedDataset.add(record.getValue());
+        }
+        approveLineData.setDataset(approvedDataset);
+        lineChartData.setXAxisData(xAxisData);
+        dataList.add(approveLineData);
+
+        LineData deniedLineData = new LineData();
+        deniedLineData.setLabel("Denied");
+        List<Number> deniedDataset = new ArrayList<>();
+        for(Map.Entry<YearMonth, Integer> record: DeniedCount.entrySet()){
+            deniedDataset.add(record.getValue());
+        }
+        deniedLineData.setDataset(deniedDataset);
+        dataList.add(deniedLineData);
+
+        lineChartData.setDataList(dataList);
+        return lineChartData;
     }
 }
